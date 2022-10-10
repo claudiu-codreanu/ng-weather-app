16ede152e0b4a99725bc33c3d1338f5c865b7d1f
Claudiu Codreanu <claudiu.codreanu@covance.com>
2022-10-10 18:31:34 -0400

    Adds connection to weather api end-point and weather service

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: "http://api.openweathermap.org/data/2.5/weather/",
+  apiKey: "da8c3694599080d7448e4d26d85cd392"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: "http://api.openweathermap.org/data/2.5/weather/",
+  apiKey: "da8c3694599080d7448e4d26d85cd392"
 };
 
 /*
