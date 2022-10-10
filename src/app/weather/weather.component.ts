d9f4b6145313e03c0bea0eb02519664759559959
Claudiu Codreanu <claudiu.codreanu@covance.com>
2022-10-10 18:57:52 -0400

    Adds the city search and current weather display component

---
+++ src/app/weather/weather.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from '../weather.service';
+import { Weather } from '../weather';
+
+@Component({
+  selector: 'app-weather',
+  templateUrl: './weather.component.html',
+  styleUrls: ['./weather.component.scss']
+})
+export class WeatherComponent implements OnInit {
+
+  weather: Weather | undefined;
+
+  constructor(private weatherService: WeatherService) { }
+
+  ngOnInit(): void {
+  }
+
+  search(city: string) {
+    this.weatherService.getWeather(city).subscribe(w => this.weather = w);
+  }
+
+}
