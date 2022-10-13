45d3c0a92fb4ac5cfa129e90181346b47d24344c
Claudiu Codreanu <claudiu.codreanu@covance.com>
2022-10-13 16:52:38 -0400

    Changes http to https, adds disclaimer about api key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: "http://api.openweathermap.org/data/2.5/weather/",
+  apiUrl: "https://api.openweathermap.org/data/2.5/weather/",
+
+  // please don't use the key below, generate your own at openweathermap.org, it's easy and free
   apiKey: "da8c3694599080d7448e4d26d85cd392"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,9 @@
 
 export const environment = {
   production: false,
-  apiUrl: "http://api.openweathermap.org/data/2.5/weather/",
+  apiUrl: "https://api.openweathermap.org/data/2.5/weather/",
+
+  // please don't use the key below, generate your own at openweathermap.org, it's easy and free
   apiKey: "da8c3694599080d7448e4d26d85cd392"
 };
 
